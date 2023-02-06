ff9d9c86f29b03bbdb9c0d9ad5ef94a5413a58ad
Bella Ranalli <bela.ranalli@gmail.com>
2023-02-05 22:40:10 -0300

    Corrijo error async/await, falta agregar limite

--- src/app.js
+++ src/app.js
@@ -1,9 +1,8 @@
 import express from "express";
 import { Product_Manager } from "./productManager.js";
 const app = express();
-app.use(express.urlencoded({ extended: true }));
 
-app.get('/products', (req, res) => {
+/*app.get('/products', (req, res) => {
     const { limit } = req.query
     Product_Manager.getProducts().then((products) => {
         if (products.length === 0) return res.send('No items')
@@ -14,18 +13,23 @@ app.get('/products', (req, res) => {
     }).catch(err => {
         res.send(`Ha ocurrido un error ${err} al cargar la página`)
     })
-})
+})*/
 
-app.get('/products/:productId', (req, res) => {
+app.get('/products', async (req, res) => {
+    let totalProducts = await Product_Manager.getProducts();
+    res.send(totalProducts)
+});
+
+app.get('/products/:productId', async (req, res) => {
     const id = req.params.productId
-    Product_Manager.getProductById(id).then(product => {
-        res.send(product)
-    }).catch(err => {
-        res.send(`Ha ocurrido un error ${err} al cargar la página`)
+    let productsById = await Product_Manager.getProductById(id)
+        res.send(productsById)
     })
-})
+
 
 const port = 8080;
 app.listen(port, () => {
     console.log('Server listening at Port: ', port)
 });
+
+
