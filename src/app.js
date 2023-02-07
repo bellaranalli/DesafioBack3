6301da752e8550a0b52fa02649ba55fe74b95395
Bella Ranalli <bela.ranalli@gmail.com>
2023-02-07 19:00:30 -0300

    resuelvo error de límite null y muestro error con id por fuera de los asignados

--- src/app.js
+++ src/app.js
@@ -6,23 +6,27 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/products', async (req, res) => {
     let totalProducts = await Product_Manager.getProducts();
     let limit = parseInt(req.query.limit)
-    let arraylimit = []
-    let x = 0
+    
     if (limit) {
-        while (x < limit) {
-            arraylimit.push(totalProducts[x])
-            x++
-        }
-        res.send(arraylimit)
+        let productLimit = totalProducts.slice(0, limit)
+        res.send(productLimit)
+    } else {
+        res.send(totalProducts);
     }
-     res.send(totalProducts);
 
 });
 
 app.get('/products/:productId', async (req, res) => {
     const id = parseInt(req.params.productId)
-    let productsById = await Product_Manager.getProductById(id)
-    res.send(productsById);
+    let products = await Product_Manager.getProducts()
+    let limit = products.length
+
+    if (id <= limit) {
+        let productId = products.filter(el => el.id===id) 
+        res.send(productId)
+    }else{
+        res.send({error:"El producto no existe"})
+    }
 })
 
 const port = 8080;
@@ -30,3 +34,12 @@ app.listen(port, () => {
     console.log('Server listening at Port: ', port)
 });
 
+/*let arraylimit = []
+    let x = 0
+    if (limit) {
+        while (x < limit) {
+            arraylimit.push(totalProducts[x])
+            x++
+        }
+        res.send(arraylimit)
+    }*/ //este método funciona para llamar a los objetos por límite pero si llamo a un número superior al objeto suma null.
